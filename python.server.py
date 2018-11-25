c1ea29450de137f667c9723d18615529eeab1185
andrey <tsariukandreyjs@gmail.com>
2018-11-25 20:52:20 +0200

    added python http server for local env

---
+++ python.server.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+# from tooth import *
+import time
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    __init__():
+        self.dataset_val, self.model, self.coco = prepareDatasetAndModel()
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_POST(self):
+        print('FOOOOO POST')
+        saveToFile('6vm2eenjox7j8lw.jpg', self.model, self.dataset_val)
+        print('FOOOOO POST DONE')
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        paths = {
+            '/recognize': {'status': 200},
+        }
+
+        if self.path in paths:
+            self.respond(paths[self.path])
+        else:
+            self.respond({'status': 500})
+
+    def handle_http(self, status_code, path):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        content = '''
+        <html><head><title>Title goes here.</title></head>
+        <body><p>This is a test.</p>
+        <p>You accessed path: {}</p>
+        </body></html>
+        '''.format(path)
+        return content
+
+    def respond(self, opts):
+        response = self.handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
