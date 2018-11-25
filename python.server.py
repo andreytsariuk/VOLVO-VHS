8602d618cfa6bb4f3759ba443e63fcafa8341a7c
andrey <tsariukandreyjs@gmail.com>
2018-11-26 00:33:02 +0200

    try to fix memory leaks

--- python.server.py
+++ python.server.py
@@ -11,6 +11,9 @@ queue = []
 
 
 class MyHandler(BaseHTTPRequestHandler):
+    model = None
+    dataset_val = None
+
 
     def do_HEAD(self):
         self.send_response(200)
@@ -18,7 +21,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
-        dataset_val, model, coco = prepareDatasetAndModel()
+
          # <--- Gets the size of data
         content_length = int(self.headers['Content-Length'])
         # <--- Gets the data itself
@@ -33,7 +36,7 @@ class MyHandler(BaseHTTPRequestHandler):
             my_num = bool(1) if len(queue) < 2 else bool(0)
         try:
             print('FOOOOO POST', post_body['image'])
-            saveToFile(post_body['image'], model, dataset_val)
+            saveToFile(post_body['image'], self.model, self.dataset_val)
             print('FOOOOO POST DONE')
             # live queue
             queue.remove(post_body['image'])
@@ -73,13 +76,30 @@ class MyHandler(BaseHTTPRequestHandler):
         self.wfile.write(response)
 
 
+class http_server:
+
+    def __init__(self, dataset_val, model):
+        myHandler.dataset_val = dataset_val
+        myHandler.model = model
+
+        server = HTTPServer((HOST_NAME, PORT_NUMBER), myHandler)
+        server.serve_forever()
+
+
+class main:
+    def __init__(self):
+        self.dataset_val, self.model, self.coco = prepareDatasetAndModel()
+        self.server = http_server(self.dataset_val, self.model)
+
+
 if __name__ == '__main__':
-    server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    # server_class = HTTPServer
+    # httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    # print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    # try:
+    #     httpd.serve_forever()
+    # except KeyboardInterrupt:
+    #     pass
+    # httpd.server_close()
+    # print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    m = main()
