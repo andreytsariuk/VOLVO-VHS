1cc5280eadcc525049ac27f048d33222a549e418
andrey <tsariukandreyjs@gmail.com>
2018-11-26 06:32:46 +0200

    investigate

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ import coco
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "h5-models/mask_rcnn_coco.h5")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "h5-models/tooths.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
@@ -59,21 +59,23 @@ model.load_weights(COCO_MODEL_PATH, by_name=True)
 # COCO Class names
 # Index of the class in the list is its ID. For example, to get ID of
 # the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+# class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+#                'bus', 'train', 'truck', 'boat', 'traffic light',
+#                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+#                'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+#                'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+#                'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+#                'kite', 'baseball bat', 'baseball glove', 'skateboard',
+#                'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+#                'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+#                'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+#                'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+#                'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+#                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+#                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+#                'teddy bear', 'hair drier', 'toothbrush']
+
+class_names = ['BG','Tooth','Bottom']
 
 
 # Load a random image from the images folder
